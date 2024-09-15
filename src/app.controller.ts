6394458e5a77262555d1f2322f31f4dddc0f35cb
Guxtaviko <guxtaviko@hotmail.com>
2024-09-15 12:54:45 -0300

    feat: add redirect

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Response } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { AppService } from './app.service'
+import { FastifyReply } from 'fastify'
+
+@ApiTags('App')
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get(':shortUrl')
+  async redirect(
+    @Response() res: FastifyReply,
+    @Param('shortUrl') shortUrl: string,
+  ) {
+    const url = await this.appService.redirect(shortUrl)
+    res.redirect(url, 301)
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { LinksService } from './modules/links/links.service'
+
+@Injectable()
+export class AppService {
+  constructor(private readonly linksService: LinksService) {}
+
+  async redirect(shortUrl: string): Promise<string> {
+    const link = await this.linksService.findByShortUrl(shortUrl)
+    if (!link) throw new NotFoundException('Link not found')
+
+    return link.url
+  }
+}
